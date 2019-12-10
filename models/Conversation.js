7ed676b1db101a737d8e77eb231e1bca2184a19a
thomassmith <thomassmith@Thomass-MacBook-Pro.local>
2019-12-10 21:03:25 +1100

    created conversation model

---
+++ models/Conversation.js
@@ -0,0 +1,14 @@
+// import mongoose and Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const conversationSchema = new Schema({
+    participants: {
+        type: [mongoose.Schema.Types.ObjectId],
+        required: true
+    }
+})
+
+const Conversation = mongoose.model('Conversation', conversationSchema);
+
+module.exports = Conversation;
