0c08cc68d65e028ea05f9dfbf32f1f4ef482dedb
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-12-31 11:36:08 +0800

    Revert "- created 'Individual' investorType model"
    This reverts commit 0cae3ae1396efe4a007836b897961f6fb3d6f2c5.

--- models/Profile.js
+++ models/Profile.js
@@ -34,6 +34,10 @@ const profileSchema = new Schema({
         type: Boolean,
         default: false
     },
+    investorType: {
+        type: String,
+        enum: ['individual', 'individual trustee', 'company', 'corporate trustee']
+    },
     dateStarted: {
         type: Date,
         default: Date.now
