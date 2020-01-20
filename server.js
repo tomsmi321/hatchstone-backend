9f3b26acae694087e60f20523fafd4cb3cb6351d
Chris Staudinger <chris.j.staudinger@gmail.com>
2020-01-21 08:45:03 +1100

    server PORT interoperable development/production bug fix

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const app = require('./app');
 require('dotenv').config()
 
 // define a PORT
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
+
 
 // make the app run on the specified port
 app.listen(PORT, () => {
