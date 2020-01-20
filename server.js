963eeeed0e46b3805d2500b0b69dc10a6d44de3c
Richard Sando <richardsando369@gmail.com>
2020-01-20 19:05:47 +1100

    changed port to env variable

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require('./app');
+require('dotenv').config()
 
 // define a PORT
-const PORT = 5000;
+const PORT = process.env.PORT
 
 // make the app run on the specified port
 app.listen(PORT, () => {
