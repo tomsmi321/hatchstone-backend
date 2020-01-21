b5859590a08d0736263a1fd44e01b1147bf04091
Chris Staudinger <chris.j.staudinger@gmail.com>
2020-01-21 23:37:27 +1100

    checkAuth finally working!! Messy atm. Leaving it like that so it's easy to see what's going on if there are issues protecting other routes

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -1,23 +1,28 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-// middleware for checking the use has a valid jwt token
+// middleware for checking the user has a valid jwt token
 const checkAuth = async (req, res, next) => {
-    const { token } = req.headers;
-    const data = jwt.verify(token, process.env.JWT_SECRET);
-    try {
-        const { email } = data;
-        const user = await User.findOne({email: email});
-        // attach the token and the user to subsequent request objects
-        req.user = user
-        req.token = token
-        console.log(req.user);
-        console.log(req.token);
-        next();
-    } catch(err) {
-        res.status(401).send('Not authorized to access this resource')
-    }
-
+  const { authorization } = req.headers
+  // Either localStorage or request headers is wrapping the token in ". We remove them otherwise JWT won't match
+  const token = authorization.replace(/"/g, "")
+  console.log('AUTH::::', token)
+  const data = jwt.verify(token, process.env.JWT_SECRET);
+  console.log('DATA::::', data)
+  try {
+      const { email } = data;
+      const user = await User.findOne({email: email});
+      // attach the token and the user to subsequent request objects
+      req.user = user
+      req.token = token
+      console.log(req.user);
+      console.log(req.token);
+      console.log('DONE!')
+      return res.status(200).send(user)
+      // next();
+  } catch(err) {
+      return res.status(401).send('Not authorized to access this resource')
+  }
 }
 
 module.exports = {
