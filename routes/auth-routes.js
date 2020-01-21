73a071698aa102e1e63f7783bc43668d829dc66d
Chris Staudinger <chris.j.staudinger@gmail.com>
2020-01-21 23:37:27 +1100

    checkAuth finally working!! Messy atm. Leaving it like that so it's easy to see what's going on if there are issues protecting other routes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/auth-controller');
 const { validateNewUser } = require('../middleware/validation-middleware/user-validation');
-
+const { checkAuth } = require('../middleware/auth-middleware');
 
 router.post('/register', validateNewUser, register);
 router.post('/login', login);
+router.get('/checkToken', checkAuth)
 
 module.exports = router;
