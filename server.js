26b76d88beb799148b854bc86a58a9ad82946012
Chris Staudinger <chris.j.staudinger@gmail.com>
2020-01-24 11:27:15 +1100

    Revert "Merge pull request #30 from tomsmi321/integration-test"
    This reverts commit aec887595b4a62b6f3d127e010fb690ed91e92c4, reversing
    changes made to b19b67f7957eebeb5a872358880c17ec2af5a062.

--- server.js
+++ server.js
@@ -1,29 +1,5 @@
 const app = require('./app')
-const mongoose = require('mongoose')
 require('dotenv').config()
-
-//connect to
-const mongoUri = process.env.MONGOURI
-
-// db options
-const dbOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-}
-
-const connectDb = async () => {
-  try {
-    await mongoose.connect(mongoUri, dbOptions)
-    console.log('success: mongodb connected ✅')
-  } catch (err) {
-    console.log('error: mongodb not connected 😞')
-  }
-}
-
-// connect to the db
-connectDb()
 
 // define a PORT
 const PORT = process.env.PORT || 5000
